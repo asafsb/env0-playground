e90f4c1fa143e6eea02faba8337944952618846a
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-05-07 14:43:11 -0500

    Create main.tf

---
+++ env-tf-var-test/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    }
+  }
+}
+
+variable "bucketname" {
+  type    = string
+  default = "env0-acme-bucket"
+}
+
+resource "random_string" "random" {
+  length  = 5
+  special = false
+  lower   = true
+  upper   = false
+  number  = true
+}
+  
+output "s3_bucket_name" {
+  value = "${var.bucketname}-${random_string.random.id}"
+}
