7a0e28ea1613a439ae89feaa854de889b0985413
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-07-11 01:37:21 -0500

    Update main.tf

--- build_map/rule_env/main.tf
+++ build_map/rule_env/main.tf
@@ -3,10 +3,30 @@ terraform {
     env0 = {
       source = "env0/env0"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.1.0"
+    }
   }
 }
 
+variable "refresh_date" {
+  type = string
+  default = "00/00/0000"
+}
+
+resource "random_string" "random" {
+  keepers = {
+      refresh_date = var.refresh_date
+  }
+  length           = 5
+  upper            = true
+  special          = false
+}
+
 provider "env0" {
+  api_key    = var.env0_api_key
+  api_secret = var.env0_api_secret
 }
 
 locals {
@@ -31,7 +51,7 @@ data "env0_environment" "environment" {
 
 
 resource "env0_variable_set" "project_scope_var_set" {
-  name        = "policy_variable-set"
+  name        = "policy_variable-set-${random_string.random.result}"
   description = "Rule policy ${var.policy_name}"
   scope       = "project"
   scope_id    = data.env0_project.project.id
