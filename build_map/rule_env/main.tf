515da971c94519f1029f50467795fb036aa0188f
Asaf Blubshtein <57428051+asafsb@users.noreply.github.com>
2024-07-11 01:12:06 -0500

    Update main.tf

--- build_map/rule_env/main.tf
+++ build_map/rule_env/main.tf
@@ -20,3 +20,34 @@ locals {
 output "test" {
   value = local.policy
 }
+
+data "env0_project" "project" {
+  id = var.policy_list_proj
+}
+
+data "env0_environment" "environment" {
+  id = var.policy_list_env
+}
+
+
+resource "env0_variable_set" "project_scope_var_set" {
+  name        = "policy_variable-set"
+  description = "Rule policy ${var.policy_name}"
+  scope       = "project"
+  scope_id    = data.env0_project.project.id
+
+  variable {
+    name   = "policy_map"
+    value  = local.policy
+    format = "hcl"
+    type = "terraform"
+  }
+}
+
+resource "env0_variable_set_assignment" "assignment" {
+  scope    = "environment"
+  scope_id = data.env0_environment.id
+  set_ids = [
+    env0_variable_set.project_scope_example.id
+  ]
+}
