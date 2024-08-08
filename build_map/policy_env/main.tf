ccd4cb0b75e799b834c351c23b7bc3e61421d2e4
asafsb <asafsb@gmail.com>
2024-08-07 23:38:14 -0500

    test

--- build_map/policy_env/main.tf
+++ build_map/policy_env/main.tf
@@ -1,2 +1,15 @@
-resource "null_resource" "null" {
+terraform {
+  required_providers {
+    env0 = {
+      source = "env0/env0"
+    }
+  }
+}
+
+locals {
+  policy_list = flatten([
+    for policy_var in var.policy_variables : {
+        policy = var.${policy_var}
+      }
+  ])
 }
---
+++ build_map/policy_env/variables.tf
@@ -0,0 +1,15 @@
+variable "policy_variables" {
+  type        = list
+  default = ["policy-test_policy-QUImw","policy-test_policy-1234"]
+}
+
+variable "policy-test_policy-QUImw" {
+  type        = string
+  default = "test"
+}
+
+
+variable "policy-test_policy-1234" {
+  type        = string
+  default = "test"
+}
